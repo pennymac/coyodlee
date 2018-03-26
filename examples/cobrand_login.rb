1877786a952c9ec7d699e8c0979658a0b0c5f6e7
Daniel Dyba <daniel.dyba@gmail.com>
2018-03-25 21:33:39 -0700

    Adds examples directory

---
+++ examples/cobrand_login.rb
@@ -0,0 +1,18 @@
+require 'coyodlee'
+require 'coyodlee/sessions'
+require 'dotenv/load'
+require 'pry'
+
+Coyodlee.setup do |config|
+  config.base_url = 'https://developer.api.yodlee.com/ysl/restserver/v1'
+  config.cobrand_login = ENV['YODLEE_COBRAND_LOGIN']
+  config.cobrand_password = ENV['YODLEE_COBRAND_PASSWORD']
+end
+
+cob_session = Coyodlee::Sessions::CobrandSession.new
+
+resp = cob_session.login login_name: Coyodlee.cobrand_login,
+                         password: Coyodlee.cobrand_password
+
+puts resp
+
