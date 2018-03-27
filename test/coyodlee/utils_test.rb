807805a5140115fca6b6be0a5b8da07aec5ff390
Daniel Dyba <daniel.dyba@pnmac.com>
2018-03-27 12:27:25 -0700

    Removes build_uri from Utils

--- test/coyodlee/utils_test.rb
+++ test/coyodlee/utils_test.rb
@@ -15,18 +15,4 @@ class Coyodlee::UtilsTest < Minitest::Test
     assert_equal uncapitalized_camelize('hello'), 'hello'
     assert_equal uncapitalized_camelize('one_two_three'), 'oneTwoThree'
   end
-
-  def test_build_uri
-    full_uri = build_uri('/restserver/v1/cobrand/login')
-
-    assert_equal full_uri.request_uri,
-                 '/restserver/v1/cobrand/login'
-    assert_equal full_uri.host,
-                 'developer.api.yodlee.com'
-    assert_equal full_uri.scheme,
-                 'https'
-    assert_nil full_uri.query
-    assert_equal full_uri.to_s,
-                 'https://developer.api.yodlee.com/restserver/v1/cobrand/login'
-  end
 end
