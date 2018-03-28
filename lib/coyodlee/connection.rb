e6e242f1a4df0b48fe26feb746efb262d22a1a50
Daniel Dyba <daniel.dyba@pnmac.com>
2018-03-28 14:49:29 -0700

    Introduces RequestBuilder

--- lib/coyodlee/connection.rb
+++ lib/coyodlee/connection.rb
@@ -2,16 +2,47 @@ require_relative 'uri_builder'
 require_relative 'requests'
 
 module Coyodlee
+  class RequestBuilder
+    def initialize(uri_builder)
+      @uri_builder = uri_builder
+    end
+
+    def build(method, resource_path, headers: {}, params: {}, body: nil)
+      uri = @uri_builder.build(resource_path)
+      http_constructor(method).new(uri).tap do |req|
+        add_headers(req, headers)
+        req.body = body if body
+      end
+    end
+
+    private
+
+    def add_headers(req, headers)
+      headers.each do |key, value|
+        req[key] = value
+      end
+    end
+
+    def http_constructor(method)
+      case method
+      when :get
+        Net::HTTP::Get
+      when :post
+        Net::HTTP::Post
+      end
+    end
+  end
+
   class RequestExecutor
     def initialize(facade)
       @facade = facade
     end
 
     def execute(klass, params={})
-      uri_builder = @facade.uri_builder
       http = @facade.http
       auth = @facade.session_authorization
-      req = klass.new(uri_builder: uri_builder,
+      request_builder = @facade.request_builder
+      req = klass.new(request_builder: request_builder,
                       session_authorization: auth)
               .build(params)
       http.request(req)
@@ -20,14 +51,14 @@ module Coyodlee
 
   class RequestFacade
     attr_reader :http
-    attr_reader :uri_builder
+    attr_reader :request_builder
     attr_reader :session_authorization
 
     include Requests
 
-    def initialize(http:, uri_builder:, session_authorization: nil)
+    def initialize(http:, request_builder:, session_authorization: nil)
       @http = http
-      @uri_builder = uri_builder
+      @request_builder = request_builder
       @session = session_authorization
     end
 
@@ -57,18 +88,18 @@ module Coyodlee
   class Connection
     class << self
       def create
-        new UriBuilder.new(host: Coyodlee.host)
+        new RequestBuilder.new(UriBuilder.new(host: Coyodlee.host))
       end
     end
 
-    def initialize(uri_builder)
-      @uri_builder = uri_builder
+    def initialize(request_builder)
+      @request_builder = request_builder
     end
 
     def start(&block)
       Net::HTTP.start('developer.api.yodlee.com', use_ssl: true) do |http|
         yield RequestFacade.new(http: http,
-                                uri_builder: @uri_builder)
+                                request_builder: @request_builder)
       end
     end
   end
--- lib/coyodlee/requests.rb
+++ lib/coyodlee/requests.rb
@@ -4,63 +4,74 @@ require 'json'
 module Coyodlee
   module Requests
     class GetAccountsRequest
-      def initialize(uri_builder:, session_authorization:)
-        @uri_builder = uri_builder
+      def initialize(request_builder:, session_authorization:)
+        @request_builder = request_builder
         @session_authorization = session_authorization
       end
 
       def build(_)
-        uri = @uri_builder.build('accounts')
-        Net::HTTP::Get.new(uri).tap do |req|
-          req['Accept'] = 'application/json'
-          req['Authorization'] = @session_authorization.to_s
-        end
+        @request_builder
+          .build(
+            :get,
+            'accounts',
+            headers: {
+              'Accept' => 'application/json',
+              'Authorization' => @session_authorization.to_s
+            }
+          )
       end
     end
   end
 
   class UserLoginRequest
-    def initialize(uri_builder:, session_authorization:)
-      @uri_builder = uri_builder
+    def initialize(request_builder:, session_authorization:)
+      @request_builder = request_builder
       @session_authorization = session_authorization
     end
 
     def build(login_name:, password:)
-      uri = @uri_builder.build('user/login')
-      Net::HTTP::Post.new(uri).tap do |req|
-        req.body = {
-          user: {
-            loginName: login_name,
-            password: password,
-            locale: 'en_US'
-          }
-        }.to_json
-        req['Content-Type'] = 'application/json'
-        req['Accept'] = 'application/json'
-        req['Authorization'] = @session_authorization.to_s
-      end
+      @request_builder
+        .build(
+          :post,
+          'user/login',
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => @session_authorization.to_s
+          },
+          body: {
+            user: {
+              loginName: login_name,
+              password: password,
+              locale: 'en_US'
+            }
+          }.to_json
+        )
     end
   end
 
   class CobrandLoginRequest
-    def initialize(uri_builder:, session_authorization: nil)
-      @uri_builder = uri_builder
+    def initialize(request_builder:, session_authorization: nil)
+      @request_builder = request_builder
     end
 
     def build(login_name:, password:)
-      uri = @uri_builder.build('cobrand/login')
-      Net::HTTP::Post.new(uri).tap do |req|
-        req.body = {
-          cobrand: {
-            cobrandLogin: login_name,
-            cobrandPassword: password,
-            locale: 'en_US'
-          }
-        }.to_json
-        req['Content-Type'] = 'application/json'
-        req['Accept'] = 'application/json'
-      end
+      @request_builder
+        .build(
+          :post,
+          'cobrand/login',
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          },
+          body: {
+            cobrand: {
+              cobrandLogin: login_name,
+              cobrandPassword: password,
+              locale: 'en_US'
+            }
+          }.to_json
+        )
     end
   end
-
 end
