bb780c7eb78b488bc9fac7f22214cb22a6a392eb
Daniel Dyba <daniel.dyba@gmail.com>
2018-03-28 19:02:35 -0700

    Minor changes to build method of UriBuilder

--- lib/coyodlee/uri_builder.rb
+++ lib/coyodlee/uri_builder.rb
@@ -9,8 +9,15 @@ module Coyodlee
 
     def build(resource_path, query: nil, use_ssl: true)
       uri_builder = use_ssl ? URI::HTTPS : URI::HTTP
-      revised_resource_path = resource_path.start_with?('/') ? resource_path.slice(1..-1) : resource_path
-      path = "/#{@path_prefix}/#{@cobrand}/#{@version}/#{revised_resource_path}"
+      revised_resource_path = if resource_path.start_with?('/')
+                                resource_path.slice(1..-1)
+                              else
+                                resource_path
+                              end
+      path = [@path_prefix, @cobrand, @version, revised_resource_path]
+               .compact
+               .join('/')
+               .prepend('/')
       uri_builder.build(host: @host, path: path, query: query)
     end
   end
