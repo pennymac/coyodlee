bd059bfe086970bdc42fb809d56e3e9f48a85d53
Daniel Dyba <daniel.dyba@pnmac.com>
2018-03-29 08:40:30 -0700

    Add support for several Yodlee API requests

--- lib/coyodlee/connection.rb
+++ lib/coyodlee/connection.rb
@@ -72,22 +72,15 @@ module Coyodlee
     def_delegators :@request_builder, :authorize
 
     def cobrand_login(login_name:, password:)
-      req = @request_builder
-              .build(
-                :post,
-                'cobrand/login',
-                headers: {
-                  'Content-Type' => 'application/json',
-                  'Accept' => 'application/json'
-                },
-                body: {
-                  cobrand: {
-                    cobrandLogin: login_name,
-                    cobrandPassword: password,
-                    locale: 'en_US'
-                  }
-                }.to_json
-              )
+      body = {
+        cobrand: {
+          cobrandLogin: login_name,
+          cobrandPassword: password,
+          locale: 'en_US'
+        }
+      }.to_json
+      headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+      req = @request_builder.build(:post, 'cobrand/login', headers: headers, body: body)
       execute(req)
     end
 
@@ -104,34 +97,258 @@ module Coyodlee
       execute(req)
     end
 
-    def get_accounts
+    def accounts
       headers = { 'Accept' => 'application/json' }
       req = @request_builder.build(:get, 'accounts', headers: headers)
       execute(req)
     end
 
-    def get_account_details(account_id:, container:)
+    def account_details(account_id:, container:)
       headers = { 'Accept' => 'application/json' }
       params = { container: container }
       req = @request_builder.build(:get, "accounts/#{account_id}", params: params, headers: headers)
       execute(req)
     end
 
-    def get_transactions_count(params={})
+    def transactions_count(params={})
       headers = { 'Accept' => 'application/json' }
       req = @request_builder.build(:get, 'transactions/count', params: params, headers: headers)
       execute(req)
     end
 
-    def get_provider_accounts
+    def provider_accounts
       headers = { 'Accept' => 'application/json' }
       req = @request_builder.build(:get, 'providerAccounts', headers: headers)
       execute(req)
     end
 
-    def get_transactions(params={})
+    def update_account(account_id:, body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:put, "accounts/#{account_id}", headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def delete_account(account_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:delete, "accounts/#{account_id}", headers: headers)
+      execute(req)
+    end
+
+    def add_manual_account(body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:post, 'accounts', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def investment_options(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'accounts/investmentPlan/investmentOptions', headers: headers, params: params)
+      execute(req)
+    end
+
+    def historical_balances(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'accounts/historicalBalances', headers: headers, params: params)
+      execute(req)
+    end
+
+    def holdings(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'holdings', headers: headers, params: params)
+      execute(req)
+    end
+
+    def extended_securities_info(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'holdings/securities', headers: headers, params: params)
+      execute(req)
+    end
+
+    def holding_type_list
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'holdings/holdingTypeList', headers: headers)
+      execute(req)
+    end
+
+    def asset_classification_list
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'holdings/assetClassificationList', headers: headers)
+      execute(req)
+    end
+
+    def providers(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, "providers", headers: headers, params: params)
+      execute(req)
+    end
+
+    def verify_provider_account(body:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:put, 'providerAccounts/verification', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def verification_status(provider_account_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, "providerAccounts/verification/#{provider_account_id}", headers: headers)
+      execute(req)
+    end
+
+    def update_provider_account(body:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:put, 'providerAccounts', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def delete_provider_account(provider_account_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:delete, "providerAccounts/#{provider_account_id}", headers: headers)
+      execute(req)
+    end
+
+    def provider_account_details(provider_account_id:, params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, "providerAccounts/#{provider_account_id}", headers: headers, params: params)
+      execute(req)
+    end
+
+    def add_provider_account(provider_id:, body:)
+      headers = { 'Accept' => 'application/json' }
+      params = { 'providerId' => provider_id }
+      req = @request_builder.build(:post, "providerAccounts", headers: headers, params: params, body: body)
+      execute(req)
+    end
+
+    def provider_details(provider_id:, provider_account_id:)
+      headers = { 'Accept' => 'application/json' }
+      params = { 'providerAccountId' => provider_account_id }
+      req = @request_builder.build(:get, "providers/#{provider_id}", headers: headers, params: params)
+      execute(req)
+    end
+
+    def transactions(params={})
       headers = { 'Accept' => 'application/json' }
       req = @request_builder.build(:get, 'transactions', params: params, headers: headers)
+      execute(req)
+    end
+
+    def transaction_categorization_rules
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'transactions/categories/rules', headers: headers)
+      execute(req)
+    end
+
+    def create_transaction_categorization_rule(body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:post, 'transactions/categories/rules', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def update_transaction_categorization_rule(rule_id:, body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:put, "transactions/categories/rules/#{rule_id}", headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def delete_transaction_categorization_rule(rule_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:put, "transactions/categories/rules/#{rule_id}", headers: headers)
+      execute(req)
+    end
+
+    def run_transaction_categorization_rule(rule_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:post, "transactions/categories/rules/#{rule_id}", headers: headers)
+      execute(req)
+    end
+
+    def run_all_transaction_categorization_rules
+      params = { 'action' => 'run' }
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:post, 'transactions/categories/rules', headers: headers, params: params)
+      execute(req)
+    end
+
+    def delete_transaction_category(category_id:)
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:delete, "transactions/categories/#{category_id}", headers: headers)
+      execute(req)
+    end
+
+    def update_transaction_category(category_id:, params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:put, "transactions/categories/#{category_id}", headers: headers, params: params)
+      execute(req)
+    end
+
+    def transaction_category_list
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'transactions/categories', headers: headers)
+      execute(req)
+    end
+
+    def create_category(body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:post, 'transactions/categories', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def update_category(body:)
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      req = @request_builder.build(:put, 'transactions/categories', headers: headers, body: body.to_json)
+      execute(req)
+    end
+
+    def transactions(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'transactions', headers: headers, params: params)
+      execute(req)
+    end
+
+    def statements(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'statements', headers: headers, params: params)
+      execute(req)
+    end
+
+    def transaction_summary(group_by:, params={})
+      params['groupBy'] = group_by
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'derived/transactionSummary', headers: headers, params: params)
+      execute(req)
+    end
+
+    def holding_summary(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'derived/holdingSummary', headers: headers, params: params)
+      execute(req)
+    end
+
+    def networth_summary(params={})
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'derived/networth', headers: headers, params: params)
+      execute(req)
+    end
+
+    def extract_events(event_name:, from_date:, to_date:)
+      params = {
+        'eventName' => event_name,
+        'fromDate' => fromDate,
+        'toDate' => toDate
+      }
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'dataExtracts/events', headers: headers, params: params)
+      execute(req)
+    end
+
+    def user_data(login_name:, from_date:, to_date:)
+      params = {
+        'loginName' => login_name,
+        'fromDate' => fromDate,
+        'toDate' => toDate
+      }
+      headers = { 'Accept' => 'application/json' }
+      req = @request_builder.build(:get, 'dataExtracts/userData', headers: headers, params: params)
       execute(req)
     end
 
