1096536392c275896897c9ee846dc46fd3490de0
Daniel Dyba <daniel.dyba@pnmac.com>
2018-04-02 09:12:50 -0700

    Fix bug in examples demo.rb

--- examples/demo.rb
+++ examples/demo.rb
@@ -5,7 +5,6 @@ require 'dotenv/load'
 require 'pry'
 
 Coyodlee.setup do |config|
-  config.base_url = 'https://developer.api.yodlee.com/ysl/restserver/v1'
   config.host = 'developer.api.yodlee.com'
   config.cobrand_login = ENV['YODLEE_COBRAND_LOGIN']
   config.cobrand_password = ENV['YODLEE_COBRAND_PASSWORD']
