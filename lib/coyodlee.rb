0d0bf9a02ceeff49e5b570c92a14c42f296a98cc
Daniel Dyba <daniel.dyba@gmail.com>
2018-04-22 10:15:46 -0700

    Add fastlink app ids and fastlink url to configuration object

--- lib/coyodlee.rb
+++ lib/coyodlee.rb
@@ -17,6 +17,12 @@ module Coyodlee
     # The password of your cobrand
     # @return [String] The password of your cobrand
     attr_accessor :cobrand_password
+    # A comma-separated string of FastLink App IDs
+    # @return [String] A comma-separated string of FastLink App IDs
+    attr_accessor :fastlink_app_ids
+    # The FastLink url
+    # @return [String] The FastLink url
+    attr_accessor :fastlink_url
 
     # The method to configure Yodlee parameters. Use this to set the global parameters such as {Yodlee.base_url}, {Yodlee.cobrand_login}, and {Yodlee.cobrand_password}
     # @yieldparam config [Yodlee] The Yodlee object
